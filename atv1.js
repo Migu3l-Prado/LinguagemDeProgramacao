900bd6cbf5244498ba120ae7c7dd9423e62b3aa4
Jose Prado <miguelpsousa3@gmail.com>
2023-09-01 10:57:52 -0300

    adicionando arquivo da atividade 1 do teams

---
+++ atv1.js
@@ -0,0 +1,4 @@
+const salario = 1500
+const aumento = 25
+const novoSalario = salario + (salario * (aumento/100))
+console.log (`o seu sálario anterior é ${salario} e com o aumento fica ${novoSalario}`)
